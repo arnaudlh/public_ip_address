21d7f5b481639f81e7e7af2c3d5b4acb12d06332
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2019-07-21 10:54:04 +0800

    Extended comment on variables

--- variables.tf
+++ variables.tf
@@ -1,44 +1,51 @@
 variable "ip_address_name" {
-  
+  description = "(Required) name of the IP address to be created"
 }
 
 variable "location" {
-  
+  description = "(Required) Location of the IP address to be created"
 }
 
 variable "tags" {
+  description = "(Required) Tags to be applied to the IP address to be created"
   
 }
 
 variable "ip_address_rg" {
+  description = "(Required) Resource group name of the IP address to be created"
   
 }
 
 variable "diagnostics_map" {
-  
+  description = "(Required) Storage account and Event Hub for the IP address diagnostics"  
+
 }
 
 variable "log_analytics_workspace" {
+  description = "(Required) Log Analytics data for the IP address diagnostics"
   
 }
 
 variable "allocation_method" {
-  
+  description = "(Required) Allocation method of the public IP Address - static or dynamic"  
+
 }
 
-
 variable "sku" {
-  
+  description = "(Required) SKU of the IP Address to be created - either basic or premium"  
+
 }
 
 variable "dns_prefix" {
-  
+  description = "(Optional) Prefix for DNS name of the public IP address to be deployed" 
+
 }
 
 variable "timeout" {
-  
+  description = "(Optional) Timeout for the public IP address to be deployed"  
+  default = 4
 }
 
 variable "zones" {
-  description = "Zones where the PIP is deployed"  
+  description = "(Required) Azure Availability Zone where the public IP address will be deployed"  
 }
